9496bbb80d4678c42f6985a9fd8c151ff45f8f5a
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-03-02 21:46:34 +0530

    In "lineOfPattern" function hardcoded symbol

--- filledRectangle.js
+++ filledRectangle.js
@@ -1,18 +1,18 @@
-const lineOfPattern = function(count, symbol) {
-  return symbol.repeat(count);
+const lineOfPattern = function(count) {
+  return "*".repeat(count);
 }
 
 const filledRectangle = function(height, width) {
   let pattern = "";
-  for (i = height; i > 0 && width !== 0; i--){
-    pattern = pattern + lineOfPattern(width, "*") + "\n";
+  for (i = height; i > 0 && width !== 0; i--) {
+    pattern = pattern + lineOfPattern(width) + "\n";
   }
   return pattern;
 }
 
 const main = function() {
   const height = 3;
-  const width = 0;
+  const width = 1;
   console.log(filledRectangle(height, width));
 }
 
